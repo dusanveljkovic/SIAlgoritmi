79a55382e4c23421268989b74677dafafbb06f4f
dusanveljkovic <dusanveljkovickv@gmail.com>
2025-11-21 18:35:31 +0100

    show exam

--- apps/algoritmi_web/assets/js/app.js
+++ apps/algoritmi_web/assets/js/app.js
@@ -26,11 +26,6 @@ import {hooks as colocatedHooks} from "phoenix-colocated/algoritmi_web"
 import topbar from "../vendor/topbar"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-const liveSocket = new LiveSocket("/live", Socket, {
-  longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken},
-  hooks: {...colocatedHooks},
-})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
@@ -38,7 +33,6 @@ window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
@@ -80,4 +74,28 @@ if (process.env.NODE_ENV === "development") {
     window.liveReloader = reloader
   })
 }
+
+let Hooks = {}
+
+Hooks.ResizeCarousel = {
+  mounted() {
+    const img = this.el.querySelector("img")
+    if(!img) return
+
+    img.onload = () => {
+      this.el.style.width = img.offsetWidth + "px"
+    }
+
+    if(img.complete) {
+      this.el.style.width = img.offsetWidth + "px"
+    }
+  }
+}
+
+const liveSocket = new LiveSocket("/live", Socket, {
+  longPollFallbackMs: 2500,
+  params: {_csrf_token: csrfToken},
+  hooks: Hooks,
+})
+liveSocket.connect()
 
